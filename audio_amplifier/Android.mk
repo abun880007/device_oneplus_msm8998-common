f784e3f70f4315eb0e7a952178706bc238bf6916
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-03-03 23:02:26 +0100

    msm8998: add audio_amplifier for setting ANC parameter
    * also fix acdb id's based on log from cheeseburger beta5
    
    Change-Id: If60ca73216cdb5c628a00a3556ec7643fe351e93

--- audio_amplifier/Android.mk
+++ audio_amplifier/Android.mk
@@ -17,26 +17,12 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES := \
-    external/expat/lib \
-    external/tinyalsa/include \
-    external/tinycompress/include \
-    hardware/libhardware/include \
-    $(call include-path-for, audio-route) \
-    $(call include-path-for, audio-effects) \
-    $(call project-path-for,qcom-audio)/hal \
-    $(call project-path-for,qcom-audio)/hal/audio_extn \
-    $(call project-path-for,qcom-audio)/hal/msm8974
-
-LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/techpack/audio/include
-LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
-LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/vendor/qcom/opensource/audio-kernel/include
+    hardware/libhardware/include
 
 LOCAL_SHARED_LIBRARIES := \
     liblog \
     libutils \
-    libcutils \
-    audio.primary.$(TARGET_BOARD_PLATFORM)
+    libcutils
 
 LOCAL_SRC_FILES := audio_amplifier.c
 
